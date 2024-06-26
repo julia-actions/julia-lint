f3f7be4e6bd36b768cab95b5c5fbe9eacb15c95e
David Anthoff <anthoff@berkeley.edu>
2024-06-26 19:09:36 +0200

    Add diagnostic

--- main.jl
+++ main.jl
@@ -18,6 +18,9 @@ for file in files
 
             start_pos = position_at(text_file.content, diag.range[1])
             end_pos = position_at(text_file.content, diag.range[2])
+
+            println("start_pos $start_pos")
+            println("end_pos $end_pos")
             println("::$(diag.severity) file=$(uri2filepath(file)),line=$(start_pos[1]),endLine=$(end_pos[1]),col=$(start_pos[2]),endColumn=$(end_pos[2]),title=$(diag.source)::$(diag.message)")
         end
     end
