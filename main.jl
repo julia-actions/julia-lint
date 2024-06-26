9a77daa337ab6f6352f4a81ff18ea335f5ba201a
David Anthoff <anthoff@berkeley.edu>
2024-06-26 15:54:53 +0200

    Fix bug

--- main.jl
+++ main.jl
@@ -17,7 +17,7 @@ for file in files
             text_file = get_text_file(jw, file)
 
             start_pos = position_at(text_file.content, diag.range[1])
-            end_pos = position_at(text_file.content.diag.range[2])
+            end_pos = position_at(text_file.content, diag.range[2])
             println("::$(diag.severity) file=$(uri2filepath(file)),line=$(start_pos[1]),endLine=$(end_pos[1]),col=$(start_pos[2]),endColumn=$(end_pos[2]),title=$(diag.source)::$(diag.message)")
         end
     end
