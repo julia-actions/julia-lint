421724617cbd64b3fe23c2ebf69755640e6be49d
David Anthoff <anthoff@berkeley.edu>
2024-06-26 15:49:40 +0200

    Provide ranges

--- main.jl
+++ main.jl
@@ -13,8 +13,12 @@ for file in files
     diagnostics = get_diagnostic(jw, file)
 
     for diag in diagnostics
-        if diag.severity == :error
-            println("::error file=$(uri2filepath(file)),line=1,endLine=1,col=1,endColumn=10,title=$(diag.source)::$(diag.message)")
+        if diag.severity == :error || diag.severity == :warning
+            text_file = get_text_file(jw, file)
+
+            start_pos = position_at(text_file.content, diag.range[1])
+            end_pos = position_at(text_file.content.diag.range[2])
+            println("::$(diag.severity) file=$(uri2filepath(file)),line=$(start_pos[1]),endLine=$(end_pos[1]),col=$(start_pos[2]),endColumn=$(end_pos[2]),title=$(diag.source)::$(diag.message)")
         end
     end
 end
